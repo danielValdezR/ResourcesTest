2f5d42d151b89aa4d6b795ab902e9a290b9e5482
Daniel Valdez <24660693+danielValdezR@users.noreply.github.com>
2019-09-04 03:30:20 -0500

    Add files via upload

---
+++ ResourcesTest.Tests/UnitTestPerson.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ResourcesTest.Tests
+{
+  public class UnitTestPerson
+  {
+    [Fact]
+    public void FullNameTest()
+    {
+      Person person = new Person { FirstName = "John", LastName = "Wick" };
+
+      Assert.Equal(person.FirstName + " " + person.LastName, person.FullName);
+    }
+  }
+}
